1f412cd63e11472e5c47abd897e38d191239af7c
Quang-Linh LE <linktohack@gmail.com>
2015-05-09 19:03:33 +0200

    Bump to v0.0.2

--- highlight-selection.el
+++ highlight-selection.el
@@ -4,7 +4,7 @@
 
 ;; Author: Quang-Linh LE <linktohack@gmail.com>
 ;; URL: http://github.com/linktohack/highlight-selection
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Keywords: highlight selection highlight-selection
 ;; Package-Requires: ()
 
